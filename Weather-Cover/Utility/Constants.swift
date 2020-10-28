5503cd4a78bd15bd53bf912107704cd3690afe07
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-10-28 12:50:59 +0500

    Project Constants Containing File Added

---
+++ Weather-Cover/Utility/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Weather-Cover
+//
+//  Created by Ahsan Khalil🤕 on 28/10/2020.
+//
+
+import Foundation
+
+struct Constants {
+    static let PlacesAPIKey = "AIzaSyAC20gS8mRGFZq_8EmhJxkRysOpfZUDpU4"
+}
