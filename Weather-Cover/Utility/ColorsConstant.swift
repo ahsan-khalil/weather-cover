714e12074ade28fb4ebce2cc028646ca4f9553f9
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-11-02 14:57:48 +0500

    Application UI Colors file added

---
+++ Weather-Cover/Utility/ColorsConstant.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsConstant.swift
+//  Weather-Cover
+//
+//  Created by Ahsan Khalil🤕 on 28/10/2020.
+//
+
+import Foundation
+import UIKit
+
+struct ColorsConstant {
+    static let backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1) //"#F7F7F7"
+    static let textColor = #colorLiteral(red: 0.1490196078, green: 0.1921568627, blue: 0.2666666667, alpha: 1) //"#263144"
+    static let activeLinkColor = #colorLiteral(red: 0.3176470588, green: 0.4274509804, blue: 1, alpha: 1) // "#516DFF"
+    static let secondayColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) //"#FFFFFF"
+    static let Fulltransparent = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 0)
+}
