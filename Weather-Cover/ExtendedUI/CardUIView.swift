7a39ec4a3c491e0a7225595a6a9026c721956643
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-11-02 14:52:55 +0500

    Extend UIView (CardView) added

---
+++ Weather-Cover/ExtendedUI/CardUIView.swift
@@ -0,0 +1,31 @@
+//
+//  CardUIView.swift
+//  Weather-Cover
+//
+//  Created by Ahsan Khalil🤕 on 30/10/2020.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class CardUIView: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0
+    @IBInspectable var shadowOffsetWidth: CGFloat = 0
+    @IBInspectable var shadowOffsetHeight: CGFloat = 0
+    @IBInspectable var shadowColor: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity: Float = 0
+    @IBInspectable var shadowRadius: CGFloat = 0
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight)
+        layer.shadowColor = shadowColor.cgColor
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: shadowRadius)
+        layer.shadowPath = shadowPath.cgPath
+    }
+}
