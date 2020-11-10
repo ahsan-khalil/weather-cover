c3670aee2b871f7961e55d742866449ca7cdcd16
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-11-10 18:09:53 +0500

    Error Handler for Data fetch from core data implemented

---
+++ Weather-Cover/Utility/ErrorHandler.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorHandler.swift
+//  Weather-Cover
+//
+//  Created by Ahsan Khalil🤕 on 09/11/2020.
+//
+
+import Foundation
+enum ErrorHandlerEnum {
+    case noInternet
+    case nullPointerException
+    case noError
+    case cityNameIsNotCorrect
+    case cityAllreadyExists
+}
